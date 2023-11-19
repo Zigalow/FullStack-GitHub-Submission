b37b79560c3623fdc566a11a2404d4d9ea8043f3
Zigalow <s224302@dtu.dk>
2023-11-19 01:34:56 +0100

    part1.5 done - changed the data, so that course is an object that holds all information in it

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -22,32 +22,34 @@ const Content = (props) => {
 
 const Total = (props) => {
     return (
-        <p>Number of exercises {props.partszzz[0].exercises + props.partszzz[1].exercises + props.partszzz[2].exercises}</p>
+        <p>Number of
+            exercises {props.partszzz[0].exercises + props.partszzz[1].exercises + props.partszzz[2].exercises}</p>
     )
 };
 
 const App = () => {
-        const course = "Half Stack application development"
-        const parts = [
-            {
-                name: "Fundamentals of React",
-                exercises: 10
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7
-            },
-            {
-                name: "State of a component",
-                exercises: 14
-            },
-        ]
-
+        const course = {
+            name: "Half Stack application development",
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14
+                },
+            ]
+        }
         return (
             <div>
-                <Header course={course}/>
-                <Content partzzz={parts}/>
-                <Total partszzz={parts}/>
+                <Header course={course.name}/>
+                <Content partzzz={course.parts}/>
+                <Total partszzz={course.parts}/>
             </div>
         )
     }
