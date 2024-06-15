cc2d9174b64225a9ffa07212f9edb31c00b8be12
Zigalow <s224302@dtu.dk>
2024-06-15 15:46:00 +0200

    Done with 1.5

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,7 +1,7 @@
 const Header = (props) => {
     return (
         <div>
-            <h1>{props.course}</h1>
+            <h1>{props.course.name}</h1>
         </div>
     )
 }
@@ -9,12 +9,11 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <div>
-            <Part part={props.parts[0]}/>
-            <Part part={props.parts[1]}/>
-            <Part part={props.parts[2]}/>
+            <Part part={props.course.parts[0]}/>
+            <Part part={props.course.parts[1]}/>
+            <Part part={props.course.parts[2]}/>
         </div>
     )
-
 }
 
 const Part = (props) => {
@@ -28,38 +27,39 @@ const Part = (props) => {
 const Total = (props) => {
     return (
         <div>
-            <p>Number of exercises {props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}</p>
+            <p>Number of
+                exercises {props.course.parts[0].exercise + props.course.parts[1].exercise + props.course.parts[2].exercise}</p>
         </div>
     )
 }
 
 const App = () => {
-    // Header
-    const course = 'Half Stack application development'
 
-    // Array of body
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercise: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercise: 7
-        },
-        {
-            name: 'State of a component',
-            exercise: 14
-        }
-    ]
-
+    // Course object
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercise: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercise: 7
+            },
+            {
+                name: 'State of a component',
+                exercise: 14
+            }
+        ]
+    }
     // Total
 
     return (
         <div>
             <Header course={course}/>
-            <Content parts={parts}/>
-            <Total parts={parts}/>
+            <Content course={course}/>
+            <Total course={course}/>
         </div>
     )
 }
