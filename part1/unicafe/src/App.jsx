318e45ff014d8900e38a8312c001ca20b933f9a4
Zigalow <s224302@dtu.dk>
2024-06-25 22:12:47 +0200

    Done with 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,30 +10,54 @@ const Button = ({handleClick, text}) => {
     )
 }
 
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({good, neutral, bad, all}) => {
 
     return (
         <>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
         </>
     )
+}
+
+const Average = ({good, bad, all}) => {
+    if (all !== 0) {
+        const average = ((1 * good) + (-1 * bad)) / all
+
+        return (
+            <p>average {average}</p>
+        )
+    }
+}
+
+const Positive = ({good, all}) => {
+
+    const positive = good / all * 100
+    return (
+        <p>positive {positive} %</p>
+    )
+
 }
 
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
 
     const handleGood = () => {
         setGood(good + 1)
+        setAll(all + 1)
     }
     const handleNeutral = () => {
         setNeutral(neutral + 1)
+        setAll(all + 1)
     }
     const handleBad = () => {
         setBad(bad + 1)
+        setAll(all + 1)
     }
 
     return (
@@ -46,7 +70,10 @@ const App = () => {
             <Button handleClick={handleBad} text={"bad"}/>
 
             <Header header={"statistics"}/>
-            <Statistics good={good} bad={bad} neutral={neutral}/>
+            <Statistics good={good} bad={bad} neutral={neutral} all={all}/>
+
+            <Average good={good} bad={bad} all={all}/>
+            <Positive good={good} all={all} />
         </div>
     )
 }
