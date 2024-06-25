c8bbc085afa96af5f0b4f9815fad3c1d9c1d1928
Zigalow <s224302@dtu.dk>
2024-06-25 21:58:46 +0200

    Done with 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,52 @@
 import {useState} from "react";
 
+const Header = ({header}) => <><h1>{header}</h1></>
+
+const Button = ({handleClick, text}) => {
+    return (
+        < button
+            onClick={handleClick}> {text}
+        </button>
+    )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+
+    return (
+        <>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+        </>
+    )
+}
 
 const App = () => {
-    
-    
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGood = () => {
+        setGood(good + 1)
+    }
+    const handleNeutral = () => {
+        setNeutral(neutral + 1)
+    }
+    const handleBad = () => {
+        setBad(bad + 1)
+    }
+
     return (
+
         <div>
-            code here
+            <Header header={"give feedback"}/>
+
+            <Button handleClick={handleGood} text={"good"}/>
+            <Button handleClick={handleNeutral} text={"neutral"}/>
+            <Button handleClick={handleBad} text={"bad"}/>
+
+            <Header header={"statistics"}/>
+            <Statistics good={good} bad={bad} neutral={neutral}/>
         </div>
     )
 }
