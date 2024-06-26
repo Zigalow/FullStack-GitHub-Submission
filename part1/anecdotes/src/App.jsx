e0ed5e3a36248e0b0674b42ecfda254bf807fa6a
Zigalow <s224302@dtu.dk>
2024-06-26 02:40:14 +0200

    Done with last exercise of part1 - 1.14 (Also added comments to last exercise)

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,16 +1,26 @@
 import {useState} from 'react'
 
+// Taken from developer.mozilla.org
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min)
     const maxFloored = Math.floor(max)
 
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // Random between min (inclusive) and max (exclusive) 
 }
 
 const Button = ({handleClick, text}) => {
     return (
         <>
             <button onClick={handleClick}>{text}</button>
+        </>
+    )
+}
+
+const Anecdote = ({anecdote, votes}) => {
+    return (
+        <>
+            <p>{anecdote}</p>
+            <p>has {votes} votes</p>
         </>
     )
 }
@@ -28,27 +38,43 @@ const App = () => {
         'The only way to go fast, is to go well.'
     ]
 
-    const [selected, setSelected] = useState(1)
-    const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+    const [selectedEntry, setSelectedEntry] = useState(0)                                 // Gets and sets the current anecdote
+    const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))       // Array of votes. Each entry correspond to anecdote entry in array
+    const [mostVotedEntry, setMostVotedEntry] = useState(0)                     // Gets and sets the current anecdote with the most votes
 
     const selectAnecdote = () => {
-        setSelected(getRandomInt(0, anecdotes.length))
+        setSelectedEntry(getRandomInt(0, anecdotes.length))
     }
 
     const handleVote = () => {
+//
+        const copiedVotes = [...votes]                                             // Copies the old array
 
-        const copiedVotes = [...votes]
+        copiedVotes[selectedEntry] += 1                                                        // Increments value at array entry/index                                 
+        setVotes(copiedVotes)                                                             // Sets/updates votes of the newly copied array
 
-        copiedVotes[selected] += 1
-        setVotes(copiedVotes)
+        handleMostVotes(copiedVotes)                                                      // THe newly updated array gets passed, so that the function can operate on the correct data
+                                                                                          // This is because the state is being updated asynchronously -  i.e. not immediately but "at some point" before the component is rendered again.)
+    }
+
+    const handleMostVotes = (updatedVotes) => {
+        let tempMostVotedEntry = mostVotedEntry
+        for (let i = 0; i < updatedVotes.length; i++) {
+            if (updatedVotes[i] > updatedVotes[mostVotedEntry]) {                         // If the 'i' entry now has most vote
+                tempMostVotedEntry = i                                                    // the temporary variable will be updated
+            }
+        }
+        setMostVotedEntry(tempMostVotedEntry)                                             // Sets the most voted entry
     }
 
     return (
         <div>
-            <p>{anecdotes[selected]}</p>
-            <p>has {votes[selected]} votes</p>
+            <h1>Anecdote of the day</h1>
+            <Anecdote anecdote={anecdotes[selectedEntry]} votes={votes[selectedEntry]}/>
             <Button handleClick={handleVote} text={"vote"}/>
             <Button handleClick={selectAnecdote} text={"next anecdote"}/>
+            <h1>Anecdote with most votes</h1>
+            <Anecdote anecdote={anecdotes[mostVotedEntry]} votes={votes[mostVotedEntry]}/>
         </div>
     )
 }
