1814260f87ef5edc8d171f5946974bcd294d081f
Zigalow <s224302@dtu.dk>
2026-02-05 23:39:15 +0100

    Completed exercise 3.1, 3.2

---
+++ part3/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const infoText = persons.length === 0
+    ? `Phonebook does not currently have info on any people`
+    : persons.length === 1
+        ? `Phonebook has info for 1 person`
+        : `Phonebook has info for ${persons.length} people`
+
+
+
+
+
+app.get('/info', (request, response) => {
+
+    const currentDate = new Date().toLocaleString();
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+
+    response.send(`
+          <div>
+                <p>${infoText}</p>
+                <p>${Date()}</p>
+            </div>
+           `)
+})
+
+
+
+
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
