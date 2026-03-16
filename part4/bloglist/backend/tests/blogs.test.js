d95249024dc3aea049ccd0d31e69c780bc298171
Zigalow <s224302@dtu.dk>
2026-03-16 22:27:35 +0100

    Completed 4.4, 4.5, 4.6

--- part4/bloglist/backend/tests/blogs.test.js
+++ part4/bloglist/backend/tests/blogs.test.js
@@ -2,9 +2,139 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-    const blogs = []
+const blogs = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+    }
+]
 
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    }
+]
+
+
+const listWithManyBlogs = [
+    { ...listWithOneBlog[0], likes: 10 },
+    { ...listWithOneBlog[0], likes: 15 },
+    { ...listWithOneBlog[0], likes: 20 },
+    { ...listWithOneBlog[0], likes: 25 },
+    { ...listWithOneBlog[0], likes: 30 },
+    { ...listWithOneBlog[0], likes: 35 },
+    { ...listWithOneBlog[0], likes: 40 },
+    { ...listWithOneBlog[0], likes: 45 },
+    { ...listWithOneBlog[0], likes: 50 }
+]
+
+
+test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
-}) 
+})
+
+
+describe('total likes', () => {
+
+
+    test('when list has only one blog, equals the like of that', () => {
+
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+
+    })
+
+    test('when total likes of blogs is 0, equals 0', () => {
+
+
+        const listWithZeroLikesBlogs = [
+            { ...listWithOneBlog[0], likes: 0 },
+            { ...listWithOneBlog[0], likes: 0 },
+            { ...listWithOneBlog[0], likes: 0 }
+        ]
+
+        const result = listHelper.totalLikes(listWithZeroLikesBlogs)
+        assert.strictEqual(result, 0)
+
+    })
+
+    test('calculates total likes of list with many blogs correclty', () => {
+
+
+
+
+        const result = listHelper.totalLikes(listWithManyBlogs)
+        assert.strictEqual(result, 270)
+    })
+
+    describe('favorite blog', () => {
+        test('equals blog with most likes from list with many blogs')
+
+        const result = listHelper.favoriteBlog(blogs);
+        assert.deepStrictEqual(result, blogs[2])
+    })
+
+
+    describe('most blog', () => {
+        test('equals author and number of blogs for author with most amount of blogs from lits with many blogs')
+
+        const result = listHelper.mostBlogs(blogs);
+        const expectedResult = {
+            author: "Robert C. Martin",
+            blogs: 3
+        }
+        assert.deepStrictEqual(result, expectedResult)
+    })
+
+
+})
--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -3,7 +3,74 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+
+
+    let totalLikes = 0;
+
+
+    blogs.forEach(b => {
+        totalLikes = totalLikes + b.likes
+    });
+
+    return totalLikes;
+}
+
+const favoriteBlog = (blogs) => {
+    let favoriteBlog = blogs[0];
+
+
+    blogs.forEach(b => {
+        if (b.likes > favoriteBlog.likes) {
+            favoriteBlog = b
+        }
+    })
+
+    return favoriteBlog;
+}
+
+const mostBlogs = (blogs) => {
+
+    let aggregatedBlogsWithAuthors = [];
+
+
+    blogs.forEach(b => {
+        if (aggregatedBlogsWithAuthors.some(obj => obj.author === b.author)) {
+            console.log(`Author: ${b.author} is in list`);
+
+            aggregatedBlogsWithAuthors = aggregatedBlogsWithAuthors.map(obj =>
+                obj.author === b.author
+                    ? { author: obj.author, blogs: obj.blogs + 1 }
+                    : obj
+            )
+        }
+        else {
+            console.log(`Adding author: ${b.author} to list`);
+            aggregatedBlogsWithAuthors = aggregatedBlogsWithAuthors.concat({ author: b.author, blogs: 1 })
+
+        }
+        console.log("Status of list: ", aggregatedBlogsWithAuthors);
+
+    })
+    let authorWithMostBlogs = aggregatedBlogsWithAuthors[0];
+
+
+
+    aggregatedBlogsWithAuthors.forEach(e => {
+        if (e.blogs > authorWithMostBlogs.blogs) {
+            authorWithMostBlogs = e;
+        }
+    });
+
+
+    return authorWithMostBlogs;
+}
+
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+
 }
