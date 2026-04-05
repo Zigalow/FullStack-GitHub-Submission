7350a5ffe00e65b80a57b377d31fdc78f274ac64
Zigalow <s224302@dtu.dk>
2026-04-05 03:37:20 +0200

    Completed exercise 4.13

--- part4/bloglist/backend/tests/blog_api.test.js
+++ part4/bloglist/backend/tests/blog_api.test.js
@@ -118,6 +118,26 @@ test('when adding a blog without a \'url\' property, server responds with 400', 
     assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 })
 
+
+test('deletion of a blog succeeds with status code 204 if id is valid', async () => {
+
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToBeDeleted = blogsAtStart[0]
+
+
+    await api.delete(`/api/blogs/${blogToBeDeleted.id}/`).expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+
+    const ids = blogsAtEnd.map(b => b.id)
+
+    assert(!ids.includes(blogToBeDeleted.id))
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+})
+
+
 after(async () => [
     await mongoose.connection.close()
 ])
